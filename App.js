552d03abb23603d990d6de7af9b520b7d536ff16
Gowri239 <gowrimopuru14@gmail.com>
2025-04-02 07:59:50 +0530

    refactor: convert into normal function syntax

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 
 const Title = () => <h1>Namaste React</h1>;
 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1 className="heading">Namste function components</h1>
-  </div>
-);
+const HeadingComponent = function () {
+  return (
+    <div id="container">
+      <Title />
+      <h1 className="heading">Namste function components</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

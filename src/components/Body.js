7a8eedf1cd9ab3ede0380f3a44dd1c956b5018fe
Gowri239 <gowrimopuru14@gmail.com>
2025-04-03 18:42:02 +0530

    add: filter functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
 import RestaurentCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurents, setListOfRestaurents] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRestaurants = listOfRestaurents.filter(
+              (restaurant) => restaurant.data.avgRating > 4
+            );
+            setListOfRestaurents(filteredRestaurants);
+          }}
+        >
+          Top rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurents.map((restaurant) => (
           <RestaurentCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>

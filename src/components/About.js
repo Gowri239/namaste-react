38deef3b3320989a5b718748f7f7e23579ed8009
Gowri239 <gowrimopuru14@gmail.com>
2025-04-05 10:01:24 +0530

    add: routing

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h1>This is Namaste Food App</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact us for food</h1>
+    </div>
+  );
+};
+
+export default Contact;

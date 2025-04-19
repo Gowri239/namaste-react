dcb6490b8744f1cf4f19d3f53785741a15bb7a90
Gowri239 <gowrimopuru14@gmail.com>
2025-04-19 18:29:50 +0530

    bug fixes

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,14 +1,14 @@
 import { MENU_URL } from "./constants";
 import { useEffect, useState } from "react";
 
-const useRestaurantMenu = async (resId) => {
+const useRestaurantMenu = (resId) => {
   const [resInfo, setResInfo] = useState();
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
     const resp = await fetch(MENU_URL + resId);
-    const data = resp.json();
+    const data = await resp.json();
     setResInfo(data);
   };
   return resInfo;

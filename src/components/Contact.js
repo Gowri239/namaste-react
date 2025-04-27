45d0448e08d826ae3e0cd7069f16480c5f3e80d6
Gowri239 <gowrimopuru14@gmail.com>
2025-04-27 08:29:24 +0530

    add: contact us form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,22 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact us for food</h1>
+    <div className="text-center">
+      <h1 className="font-bold text-3xl p-2 m-2">Contact Us</h1>
+      <form>
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="Message"
+        />
+        <button className="border-black p-2 m-2 bg-green-300 rounded-lg">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
